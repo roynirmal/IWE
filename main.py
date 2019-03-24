03c917074396bd725f0c1a09a27bfa9ffd366f81
roynirmal <nroy000@gmail.com>
2019-03-24 20:59:41 +0100

    only trigram

--- main.py
+++ main.py
@@ -43,9 +43,11 @@ root_rep = root_dictionary.build_input_feature(data.w2i)
 
 tri_dictionary = dictionary.Tri_Dictionary()
 tri_dictionary.build_dict(textLower, stop)
-
 tri_rep = tri_dictionary.build_input_feature(data.w2i)
-input_rep = dict([(k, tri_rep[k].tolist()+root_rep[k].tolist()) for k in tri_rep])
+
+input_rep = tri_rep
+
+# input_rep = dict([(k, tri_rep[k].tolist()+root_rep[k].tolist()) for k in tri_rep])
 
 ''' Build the model'''
 
